eae05cf11fe7bc9162695e6d4ea562eb72bd2c37
juanFdS <juan9794@gmail.com>
2019-02-08 18:59:45 -0300

    Implementada instancia de Show para a -> b

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,23 @@
-{-# LANGUAGE DataKinds, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DataKinds, TypeOperators, UndecidableInstances, FlexibleInstances, ScopedTypeVariables #-}
 
 module Lib
     ( someFunc
     ) where
 
 import GHC.TypeLits
+import Data.Typeable
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-instance TypeError (Text "Cannot 'Show' functions." :$$: 
-                    Text "Perhaps there is a missing argument?")
-         => Show (a -> b) where
-   showsPrec = error "unreachable"
+instance Show (a -> b) where
+    show _ = "<una función>"
+
+-- TODO: intentar hacer funcionar esto para filter
+-- instance (Typeable a, Typeable b) => Show (a -> b) where
+--     show _ = "Una función de tipo: "
+--         ++ show (typeOf (undefined :: a)) ++ " -> "
+--         ++ show (typeOf (undefined :: b))
 
 instance TypeError (Text "Estás usando una función como un número") => Num (a -> b) where
     (+) = error "unreacheable"
