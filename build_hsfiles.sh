3ac28496b32101b92df3171fa48d6043c86e70f0
Lucas David Traverso <lucas6246@gmail.com>
2019-03-29 02:25:52 -0300

    Improved build_hsfiles script
    Make build_hsfiles respect .gitignore
    Replace exact commit on stack.yml
    Use local pdepreludat for local development

--- build_hsfiles.sh
+++ build_hsfiles.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-find the-template -type f -not -path 'the-template/.stack-work/**' |
+git ls-files the-template |
     while read FILE; do 
         echo "{-# START_FILE ${FILE##the-template/} #-}"; 
         cat $FILE; 
         echo; 
     done | 
-        sed 's/the-template/{{name}}/g'
+        sed 's/the-template/{{name}}/g' |
+        sed "/REMOVE THIS LINE/ d" |
+        sed 's/^# UNCOMMENT ME \(.*\)$/\1/' |
+        sed "s/REPLACE WITH CURRENT COMMIT/$(git log -1 --pretty=format:'%H')/g"
