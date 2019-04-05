348575994cbe8a2c8983d802366cfbd6e962785f
juanFdS <juan9794@gmail.com>
2019-04-05 18:21:31 -0300

    Add a lot instances for custom type errors

--- src/PdePreludat.hs
+++ src/PdePreludat.hs
@@ -117,20 +117,51 @@ a / b = toFractional a P./ toFractional b
 --         ++ show (typeOf (undefined :: a)) ++ " -> "
 --         ++ show (typeOf (undefined :: b))
 
-instance TypeError (Text "Estás usando una función como un número") => Num (a -> b) where
-    (+) = error "unreacheable"
-    (*) = error "unreacheable"
-    abs = error "unreacheable"
-    signum = error "unreacheable"
-    fromInteger = error "unreacheable"
-    negate = error "unreacheable"
-instance TypeError (Text "Estás operando una lista con un número") => Num [a] where
-    (+) = error "unreacheable"
-    (*) = error "unreacheable"
-    abs = error "unreacheable"
-    signum = error "unreacheable"
-    fromInteger = error "unreacheable"
-    negate = error "unreacheable"
+-- Custom Type Errors
+
+--- Para funciones
+
+type ErrorNumeroXFuncion = Text "Estás usando una función como un número o un número como una función."
+type ErrorOrdenableXFuncion = Text "Las funciones no se pueden ordenar ni comparar."
+type ErrorEnumerableXFuncion = Text "Las funciones no son enumerables."
+
+instance TypeError (ErrorNumeroXFuncion) => Num (a -> b)
+instance TypeError ErrorOrdenableXFuncion => Ord (a -> b)
+instance TypeError ErrorOrdenableXFuncion => Eq (a -> b)
+instance TypeError ErrorNumeroXFuncion => Floating (a -> b)
+instance TypeError ErrorNumeroXFuncion => Fractional (a -> b)
+instance (TypeError ErrorNumeroXFuncion,
+          TypeError ErrorEnumerableXFuncion,
+          TypeError ErrorOrdenableXFuncion) => Integral (a -> b)
+instance (TypeError ErrorNumeroXFuncion, TypeError ErrorOrdenableXFuncion) => RealFrac (a -> b)
+instance (TypeError ErrorNumeroXFuncion, TypeError ErrorOrdenableXFuncion) => RealFloat (a -> b)
+instance (TypeError ErrorNumeroXFuncion, TypeError ErrorOrdenableXFuncion) => Real (a -> b)
+instance (TypeError ErrorEnumerableXFuncion) => Enum (a -> b)
+--instance Integral
+
+---Para listas
+type ErrorNumeroXLista = Text "Estás usando una lista como un número o un número como una lista."
+type ErrorEnumerableXLista = Text "Las listas no son enumerables."
+
+instance TypeError ErrorNumeroXLista => Num [a]
+instance TypeError ErrorNumeroXLista => Floating [a]
+instance TypeError ErrorNumeroXLista => Fractional [a]
+instance (Ord a, TypeError ErrorEnumerableXLista, TypeError ErrorNumeroXLista) => Integral [a]
+instance (Ord a, TypeError ErrorNumeroXLista) => RealFrac [a]
+instance (Ord a, TypeError ErrorNumeroXLista) => RealFloat [a]
+instance (Ord a, TypeError ErrorNumeroXLista) => Real [a]
+instance (TypeError ErrorNumeroXLista) => Enum [a]
+
+---Para caracteres
+type ErrorNumeroXCaracter = Text "Estás usando una caracter como un número o un número como un caracter."
+
+instance TypeError ErrorNumeroXCaracter => Num Char
+instance TypeError ErrorNumeroXCaracter => Floating Char
+instance TypeError ErrorNumeroXCaracter => Fractional Char
+instance TypeError ErrorNumeroXCaracter => Integral Char
+instance TypeError ErrorNumeroXCaracter => RealFrac Char
+instance TypeError ErrorNumeroXCaracter => RealFloat Char
+instance TypeError ErrorNumeroXCaracter => Real Char
 
 (...) :: a
 (...) = error "Falta implementar."
